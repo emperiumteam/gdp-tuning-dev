1559ee6e3b1a18077621bea62638ee494b858b4c
Zachary Bell <team@emperium.com>
2018-07-06 17:57:13 -0600

    its getting late...should probably head home soon but i'm having too much fun

--- dealer-page/map-dropdown.js
+++ dealer-page/map-dropdown.js
@@ -2,13 +2,19 @@ $( function() {
   $("#stateSelector").selectmenu({
     change: function( event, data ) {
       $(".dealerInfoPopup").css({
-        display: "none"
+        "display": "none"
+      });
+      $(".active-dealer").css({
+        "fill": "#c5370a",
+        "stroke-width": "0",
       });
       $("#" + data.item.value + "").css({
-        fill: "#f05423",
+        "fill": "#f05423",
+        "stroke": "#f05423",
+        "stroke-width": "5px",
       });
       $("#" + data.item.value + "-content").css({
-        display: "block"
+        "display": "block"
       });
     }
   });
