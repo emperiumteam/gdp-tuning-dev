d33ed7ab9decb8e3b090f9fd1927828838292a4a
Zachary Bell <team@emperium.com>
2018-07-09 22:47:42 -0600

    dealer page

--- custom-javascript.js
+++ custom-javascript.js
@@ -2,7 +2,7 @@
 /* --------------------------------------------
 TABLE OF CONTENTS:
 1. Home Page "Select Your Truck Section"
-
+2. Dealer Page map selection
 
 ----------------------------------------------- */
 
@@ -42,4 +42,42 @@ $("#logosGroup").hover(function () {
 });
 /* ----------------------------------------------- */
 /* ------------------SELECT YOUR TRUCK END-------- */
+/* ----------------------------------------------- */
+
+
+
+/* ----------------------------------------------- */
+/* ------------------2. Dealer Page - Map--------- */
+/* ----------------------------------------------- */
+$( function() {
+  $("#canada").css({
+    "fill": "#f05423",
+    "stroke": "#f05423",
+    "stroke-width": "2px",
+  });
+  $("#canada-content").css({
+    "display": "block"
+  });
+  $("#stateSelector").selectmenu({
+    change: function( event, data ) {
+      $(".dealerInfoPopup").css({
+        "display": "none"
+      });
+      $(".active-dealer").css({
+        "fill": "#c5370a",
+        "stroke-width": "0",
+      });
+      $("#" + data.item.value + "").css({
+        "fill": "#f05423",
+        "stroke": "#f05423",
+        "stroke-width": "2px",
+      });
+      $("#" + data.item.value + "-content").css({
+        "display": "block"
+      });
+    }
+  });
+});
+/* ----------------------------------------------- */
+/* ------------------Dealer Page - Map END-------- */
 /* ----------------------------------------------- */
