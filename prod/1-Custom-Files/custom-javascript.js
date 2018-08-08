b2ae2a6518cde1234e116eb9fd08280b86595c6b
Zachary Bell <team@emperium.com>
2018-08-08 13:01:47 -0600

    yo

--- prod/1-Custom-Files/custom-javascript.js
+++ prod/1-Custom-Files/custom-javascript.js
@@ -9,14 +9,20 @@ $(document).ready(function () {
     /* ----------------------------------------------- */
     /* ------------------1. SELECT YOUR TRUCK--------- */
     /* ----------------------------------------------- */
-    
+
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js";
+    $("#wrapper").after(s);
+
     var vwTarget = -0.15;
     var vwOrigin = -1.0;
     var duration = 400;
+
     
     //Function to the css rule
     function checkSize() {
-//        var size = parseInt(document.getElementById("#powerstrokeTruck").style.right);
+        //        var size = parseInt(document.getElementById("#powerstrokeTruck").style.right);
         var size = parseInt($("#powerstrokeTruck").css("right"));
         var currentVW = $(window).width();
         var test = size / currentVW;
@@ -24,19 +30,20 @@ $(document).ready(function () {
 
         if (testTwo == '-53') {
             vwTarget = -0.53;
-        } else if(testTwo == '-49') {
+        } else if (testTwo == '-49') {
             vwTarget = -0.49;
-        } else if(testTwo == '-40') {
+        } else if (testTwo == '-40') {
             vwTarget = -0.40;
-        } else if(testTwo == '-27') {
+        } else if (testTwo == '-27') {
             vwTarget = -0.27;
-        } else if(testTwo == '-17') {
+        } else if (testTwo == '-17') {
             vwTarget = -0.17;
         } else {
             vwTarget = -0.15;
         }
     }
 
+    
     $("#logosGroup").hover(function () {
         $("#powerstrokeTruck").animate({
             right: $(window).width() * vwOrigin
@@ -77,13 +84,13 @@ $(document).ready(function () {
             right: $(window).width() * vwTarget
         }, duration);
     });
-    
+
     // run test on initial page load
     checkSize();
 
     // run test on resize of the window
     $(window).resize(checkSize);
-    
+
     /* ----------------------------------------------- */
     /* ------------------SELECT YOUR TRUCK END-------- */
     /* ----------------------------------------------- */
@@ -135,16 +142,73 @@ $(document).ready(function () {
 
     $(".manualTruckLogosFigure").find("a").click(function (e) {
         e.preventDefault();
-      	var section = $(this).attr("href");
-      	var currentWindowWidth = $(window).width();
-      	var scrollPixels = -220;
-      	if (currentWindowWidth < 768) {
-        	scrollPixels = -75;
+        var section = $(this).attr("href");
+        var currentWindowWidth = $(window).width();
+        var scrollPixels = -220;
+        if (currentWindowWidth < 768) {
+            scrollPixels = -75;
         } else {
-        	scrollPixels = -220;
+            scrollPixels = -220;
         }
-      	$("html, body").animate({
-        	scrollTop: $(section).offset().top + scrollPixels
+        $("html, body").animate({
+            scrollTop: $(section).offset().top + scrollPixels
         });
     });
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
+
+    //    SLICK RESPONSIVE CAROUSEL - http://kenwheeler.github.io/slick/
+    $('.responsive-product-carousel').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+        ]
+    });
+
+
+
+
 });
