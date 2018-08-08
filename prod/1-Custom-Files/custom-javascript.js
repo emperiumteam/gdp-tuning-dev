0ef84425a911b300c5f4e2f1d5d9ef1401f5ac8d
Zachary Bell <team@emperium.com>
2018-08-08 13:23:58 -0600

    fix logo

--- prod/1-Custom-Files/custom-javascript.js
+++ prod/1-Custom-Files/custom-javascript.js
@@ -5,21 +5,17 @@ TABLE OF CONTENTS:
 2. Dealer Page map selection
 
 ----------------------------------------------- */
+
+
 $(document).ready(function () {
     /* ----------------------------------------------- */
     /* ------------------1. SELECT YOUR TRUCK--------- */
     /* ----------------------------------------------- */
 
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = "//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js";
-    $("#wrapper").after(s);
-
     var vwTarget = -0.15;
     var vwOrigin = -1.0;
     var duration = 400;
 
-    
     //Function to the css rule
     function checkSize() {
         //        var size = parseInt(document.getElementById("#powerstrokeTruck").style.right);
@@ -43,7 +39,7 @@ $(document).ready(function () {
         }
     }
 
-    
+
     $("#logosGroup").hover(function () {
         $("#powerstrokeTruck").animate({
             right: $(window).width() * vwOrigin
@@ -169,46 +165,5 @@ $(document).ready(function () {
             }
         });
     }
-
-
-    //    SLICK RESPONSIVE CAROUSEL - http://kenwheeler.github.io/slick/
-    $('.responsive-product-carousel').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-        ]
-    });
-
-
-
 
 });
