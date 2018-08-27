dbb6aa3770bf27f63a98b2f91808ef1d4f46572f
Zachary Bell <team@emperium.com>
2018-08-27 15:51:20 -0600

    piping kits

---
+++ vue-js-test/piping-kits.js
@@ -0,0 +1,91 @@
+const app = new Vue({
+    el: '.product-item-container',
+    data: {
+        gdp_products: {
+            1: {
+                prod_truck: 'powerstroke',
+                prod_category: 'gdp-piping-kits',
+                prod_code: 'GDP67F1114-B',
+                prod_manual: '',
+                social_url: 'http%3A//gdp.emperiumdesign.com/index.php?route=information/information%26information_id=',
+                social_url_prod_cat_id: 59,
+                dealer_link: 'http://gdp.emperiumdesign.com/index.php?route=information/information&information_id=58',
+                prod_msrp: 1786.32,
+                prod_name: '2011-2014 FORD 6.7L POWERSTROKE INTERCOOLER PIPING KIT (BLACK FINISH)',
+                image_domain: 'http://gdptuning.com/image/',
+                prod_image: 'catalog/Dist_Assets/Pipe Kit/allPart.png',
+                prod_desc: ""
+            },
+            2: {
+                prod_truck: 'powerstroke',
+                prod_category: 'gdp-piping-kits',
+                prod_code: 'GDP67F1114-R',
+                prod_manual: '',
+                social_url: 'http%3A//gdp.emperiumdesign.com/index.php?route=information/information%26information_id=',
+                social_url_prod_cat_id: 59,
+                dealer_link: 'http://gdp.emperiumdesign.com/index.php?route=information/information&information_id=58',
+                prod_msrp: 1414.11,
+                prod_name: '2011-2014 FORD 6.7L POWERSTROKE INTERCOOLER PIPING KIT (RAW FINISH)',
+                image_domain: 'http://gdptuning.com/image/',
+                prod_image: 'catalog/Dist_Assets/Pipe Kit/allPart.png',
+                prod_desc: ""
+            },
+            3: {
+                prod_truck: 'powerstroke',
+                prod_category: 'gdp-piping-kits',
+                prod_code: 'GDP67F1516-B',
+                prod_manual: '',
+                social_url: 'http%3A//gdp.emperiumdesign.com/index.php?route=information/information%26information_id=',
+                social_url_prod_cat_id: 59,
+                dealer_link: 'http://gdp.emperiumdesign.com/index.php?route=information/information&information_id=58',
+                prod_msrp: 1786.32,
+                prod_name: '2015-2016 FORD 6.7L POWERSTROKE INTERCOOLER PIPING KIT (BLACK FINISH)',
+                image_domain: 'http://gdptuning.com/image/',
+                prod_image: 'catalog/Dist_Assets/Pipe Kit/allPart.png',
+                prod_desc: ""
+            },
+            4: {
+                prod_truck: 'powerstroke',
+                prod_category: 'gdp-piping-kits',
+                prod_code: 'GDP67F1516-R',
+                prod_manual: '',
+                social_url: 'http%3A//gdp.emperiumdesign.com/index.php?route=information/information%26information_id=',
+                social_url_prod_cat_id: 59,
+                dealer_link: 'http://gdp.emperiumdesign.com/index.php?route=information/information&information_id=58',
+                prod_msrp: 1414.11,
+                prod_name: '2015-2016 FORD 6.7L POWERSTROKE INTERCOOLER PIPING KIT (RAW FINISH)',
+                image_domain: 'http://gdptuning.com/image/',
+                prod_image: 'catalog/Dist_Assets/Pipe Kit/allPart.png',
+                prod_desc: ""
+            },
+            5: {
+                prod_truck: 'powerstroke',
+                prod_category: 'gdp-piping-kits',
+                prod_code: 'GDP67F1718-B',
+                prod_manual: '',
+                social_url: 'http%3A//gdp.emperiumdesign.com/index.php?route=information/information%26information_id=',
+                social_url_prod_cat_id: 59,
+                dealer_link: 'http://gdp.emperiumdesign.com/index.php?route=information/information&information_id=58',
+                prod_msrp: 1786.32,
+                prod_name: '<span class="piping-kits-diff">2017-2018</span> FORD 6.7L POWERSTROKE INTERCOOLER PIPING KIT (BLACK FINISH)',
+                image_domain: 'http://gdptuning.com/image/',
+                prod_image: 'catalog/Dist_Assets/Pipe Kit/allPart.png',
+                prod_desc: ""
+            },
+            6: {
+                prod_truck: 'powerstroke',
+                prod_category: 'gdp-piping-kits',
+                prod_code: 'GDP67F1718-R',
+                prod_manual: '',
+                social_url: 'http%3A//gdp.emperiumdesign.com/index.php?route=information/information%26information_id=',
+                social_url_prod_cat_id: 59,
+                dealer_link: 'http://gdp.emperiumdesign.com/index.php?route=information/information&information_id=58',
+                prod_msrp: 1414.11,
+                prod_name: '2017-2018 FORD 6.7L POWERSTROKE INTERCOOLER PIPING KIT (RAW FINISH)',
+                image_domain: 'http://gdptuning.com/image/',
+                prod_image: 'catalog/Dist_Assets/Pipe Kit/allPart.png',
+                prod_desc: ""
+            }
+        }
+    }
+});
