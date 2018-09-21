fdd7548bff10d705b538bdde2401be3901fb25d1
Zachary Bell <team@emperium.com>
2018-09-21 16:42:52 -0600

    image optimization :after trick // fix .active class

--- migration/1-Custom-Files/custom-javascript.js
+++ migration/1-Custom-Files/custom-javascript.js
@@ -156,7 +156,7 @@ $(document).ready(function () {
 
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function () {
-            this.classList.toggle("active");
+            this.classList.toggle("active-collapsible");
             var content = this.nextElementSibling;
             if (content.style.display === "block") {
                 content.style.display = "none";
